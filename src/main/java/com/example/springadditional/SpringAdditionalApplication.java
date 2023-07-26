c14116fbadfda73b93e98515190f74264a327296
Trieu Vu <vutrieu2002@gmail.com>
2023-07-26 15:35:50 +0700

    Spring Additional: init project

---
+++ src/main/java/com/example/springadditional/SpringAdditionalApplication.java
@@ -0,0 +1,13 @@
+package com.example.springadditional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdditionalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAdditionalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springadditional/SpringAdditionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springadditional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAdditionalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
